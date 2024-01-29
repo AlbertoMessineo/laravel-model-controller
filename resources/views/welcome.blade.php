67b72ee51732cbcc57858673cf9edc200e0d537f
AlbertoMessineo <albevorox@gmail.com>
2024-01-29 15:53:11 +0100

    feat: scaffolding

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>laravel-model-controller</title>
+    @vite('resources/js/app.js')
+</head>
+
+<body>
+    <div class="container py-5">
+        <header>
+            <div class="d-flex justify-content-center">
+                <h1>CONTROLLER LARAVEL</h1>
+            </div>
+        </header>
+
+        <img src="{{ Vite::asset('resources/img/duck.jpg') }}" alt="">
+    </div>
+
+</body>
+
+</html>
